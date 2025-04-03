aaf953dc348c9908012b14e30735297a418213c1
Brian Chau <brianchau0729@gmail.com>
2025-04-03 13:31:04 -0400

    Worked on robber

---
+++ Scripts/robber.gd
@@ -0,0 +1,31 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+var playerPos = Vector2.ZERO
+var inRange = false
+@export var animation: AnimationPlayer
+
+func _physics_process(delta: float) -> void:
+	var direction = Vector2.ZERO
+	if(!inRange):
+		direction.x = playerPos.x - self.position.x
+		direction.y = playerPos.y - self.position.y
+	else:
+		animation.play("Attack")
+	velocity = direction.normalized() * SPEED
+
+	move_and_slide()
+
+func attack():
+	pass
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if(body.name == "Player"):
+		inRange = true;
+
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	if(body.name == "Player"):
+		inRange = true;
