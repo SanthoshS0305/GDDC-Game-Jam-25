9153dfba32efd1b1632cfd4037a6eb6369d325b7
Brian Chau <bchau40@stuy.edu>
2025-04-03 22:49:58 -0400

    testing level spawning

--- Scripts/manager.gd
+++ Scripts/manager.gd
@@ -2,6 +2,18 @@ extends Node2D
 @export var player: CharacterBody2D
 @export var enemyType: String
 var enemy
+var completed = false
+var maxWaves = 5
+var curWave = 0
+var enemiesSpawned = 0
+var timeBetweenSpawns = 3
+var lastSpawn = 0
+var maxDistanceX = 100
+var maxDistanceY = 100
+var cameraX = 600
+var cameraY = 400
+var active = true
+@onready var randomizer = RandomNumberGenerator.new()
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -10,11 +22,22 @@ func _ready() -> void:
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _physics_process(delta):
+	lastSpawn += delta
+	if (lastSpawn >= timeBetweenSpawns):
+		lastSpawn -= timeBetweenSpawns
+		spawn()
 	
 func spawn():
+	var leftOrRight = randomizer.randi_range(0, 1)
+	var upOrDown = randomizer.randi_range(0, 1)
+	if (leftOrRight == 0):
+		leftOrRight = -1
+	if (upOrDown == 0):
+		upOrDown = -1
+	var distX = (randomizer.randf_range(0, maxDistanceX) + cameraX) * leftOrRight
+	var distY = (randomizer.randf_range(0, maxDistanceY) + cameraY) * upOrDown
 	var spawned = enemy.instantiate()
 	spawned.setPlayer(player)
+	spawned.position = Vector2(distX, distY)
 	add_child(spawned)
