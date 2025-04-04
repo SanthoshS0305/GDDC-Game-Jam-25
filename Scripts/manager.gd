f72f22abd7d35654629cec4332b65005f89644b7
Brian Chau <bchau40@stuy.edu>
2025-04-04 08:27:11 -0400

    Added wave mechanic and fixed spawning bug

--- Scripts/manager.gd
+++ Scripts/manager.gd
@@ -3,15 +3,18 @@ extends Node2D
 @export var enemyType: String
 var enemy
 var completed = false
+##Variables that affect the spawn timing and wave mechanic
 var maxWaves = 5
 var curWave = 0
 var enemiesSpawned = 0
 var timeBetweenSpawns = 3
 var lastSpawn = 0
+##Distances for spawning in enemies
 var maxDistanceX = 100
 var maxDistanceY = 100
 var cameraX = 600
 var cameraY = 400
+##For if the player is still alive
 var active = true
 @onready var randomizer = RandomNumberGenerator.new()
 
@@ -25,6 +28,11 @@ func _physics_process(delta):
 	if (lastSpawn >= timeBetweenSpawns):
 		lastSpawn -= timeBetweenSpawns
 		spawn()
+		print("Wave: " + str(curWave))
+		if (curWave * 5 + 10 <= enemiesSpawned):
+			curWave += 1
+			enemiesSpawned = 0
+			timeBetweenSpawns -= 0.5
 	
 func spawn():
 	var leftOrRight = randomizer.randi_range(0, 1)
@@ -37,5 +45,6 @@ func spawn():
 	var distY = (randomizer.randf_range(0, maxDistanceY) + cameraY) * upOrDown
 	var spawned = enemy.instantiate()
 	spawned.setPlayer(player)
-	spawned.position = Vector2(distX, distY)
+	spawned.position = Vector2(distX, distY) + player.position
 	add_child(spawned)
+	enemiesSpawned += 1
