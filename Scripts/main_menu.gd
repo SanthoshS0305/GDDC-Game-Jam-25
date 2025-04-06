0dde1c9d5a75e11d06b6f24b40abb7ee0f8af30e
Brian Chau <bchau40@stuy.edu>
2025-04-06 16:43:37 -0400

    Completed memento wall

---
+++ Scripts/main_menu.gd
@@ -0,0 +1,9 @@
+extends Control
+@export var startScene: String
+
+func _on_start_pressed() -> void:
+	get_tree().change_scene_to_file(startScene)
+
+
+func _on_mementos_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/Mementos.tscn")
